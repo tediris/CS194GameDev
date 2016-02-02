f37649d29005f9904ab1d63552eb7226b6608ed1
Thaminda Edirisooriya <tediris@stanford.edu>
2016-02-01 16:31:16 -0800

    Added level generation

--- Assets/Procedural/TileGen.cs
+++ Assets/Procedural/TileGen.cs
@@ -37,8 +37,15 @@ public class TileGen : MonoBehaviour {
 
 	void InitMap() {
 		map = new TileType[width, height];
-		Room r = new Room (10, 10, 5);
-		r.fillMap (map);
+		CreateRoomLayout ();
+		for (int x = 0; x < numRoomsWidth; x++) {
+			for (int y = 0; y < numRoomsHeight; y++) {
+				rooms [x, y].fillMap (map);
+				rooms [x, y].fillConnections (map);
+			}
+		}
+		//Room r = new Room (10, 10, 5);
+		//r.fillMap (map);
 	}
 
 	void SpawnMap() {
@@ -56,20 +63,54 @@ public class TileGen : MonoBehaviour {
 	
 	void CreateRoomLayout() {
 		rooms = new Room[numRoomsWidth, numRoomsHeight];
-		//int roomWidthSpacing = wid
+		int roomWidthSpacing = width / numRoomsWidth;
+		int roomHeightSpacing = height / numRoomsHeight;
+		int xOffset = maxRoomSize + 1;
+		int yOffset = maxRoomSize + 1;
 		for (int x = 0; x < numRoomsWidth; x++) {
 			for (int y = 0; y < numRoomsHeight; y++) {
 				int size = rand.Next (maxRoomSize - minRoomSize + 1) + minRoomSize;
-				rooms [x, y] = new Room (x, y, size);
+				rooms [x, y] = new Room (xOffset + x * roomWidthSpacing , yOffset + y * roomHeightSpacing, x, y, size);
 			}
 		}
 		int startX = rand.Next (numRoomsWidth);
 		int startY = rand.Next (numRoomsHeight);
 
+		RecursiveBuildMaze (startX, startY);
+	}
+
+	List<Room> GetConnectedUnvisitedRooms(int x, int y) {
+		List<Room> connected = new List<Room> ();
+		if (x > 0 && rooms [x - 1, y].connectedRooms.Count == 0) {
+			connected.Add (rooms [x - 1, y]);
+		}
+		if (x < numRoomsWidth - 1 && rooms [x + 1, y].connectedRooms.Count == 0) {
+			connected.Add(rooms[x + 1, y]);
+		}
+		if (y > 0 && rooms [x, y - 1].connectedRooms.Count == 0) {
+			connected.Add (rooms [x, y - 1]);
+		}
+		if (y < numRoomsHeight - 1 && rooms [x, y + 1].connectedRooms.Count == 0) {
+			connected.Add (rooms [x, y + 1]);
+		}
+		return connected;
+	}
+
+	void RecursiveBuildMaze(int x, int y) {
+		while (true) {
+			List<Room> connected = GetConnectedUnvisitedRooms (x, y);
+			if (connected.Count == 0)
+				return;
+			Room nextRoom = connected [rand.Next (connected.Count)];
+			rooms [x, y].connectedRooms.Add (nextRoom);
+			nextRoom.connectedRooms.Add (rooms [x, y]);
+			RecursiveBuildMaze (nextRoom.gridX, nextRoom.gridY);
+		}
 	}
 
 	public class Room {
-		int x, y;
+		public int x, y;
+		public int gridX, gridY;
 		int radius;
 		public List<Room> connectedRooms;
 
@@ -80,16 +121,40 @@ public class TileGen : MonoBehaviour {
 
 		public Shape shape;
 
-		public Room(int x, int y, int radius) {
+		public Room(int x, int y, int gridX, int gridY, int radius) {
 			this.x = x;
 			this.y = y;
+			this.gridX = gridX;
+			this.gridY = gridY;
 			this.radius = radius;
 			shape = Shape.Rect;
+			connectedRooms = new List<Room>();
 		}
 
 		public void fillMap(TileType[,] tilemap) {
 			if (shape == Shape.Rect) {
 				fillRect(tilemap);
+			}
+		}
+
+		public void fillConnections(TileType[,] tilemap) {
+			foreach (Room room in connectedRooms) {
+				int currX = x;
+				int currY = y;
+				while (currX != room.x || currY != room.y) {
+					int xDist = Mathf.Abs (currX - room.x);
+					int yDist = Mathf.Abs (currY - room.y);
+					if (xDist > yDist) {
+						// move in the x direction
+						int xDelta = (room.x - currX) / xDist;
+						currX += xDelta;
+						tilemap [currX, currY] = TileType.Empty;
+					} else {
+						int yDelta = (room.y - currY) / yDist;
+						currY += yDelta;
+						tilemap [currX, currY] = TileType.Empty;
+					}
+				}
 			}
 		}
 
