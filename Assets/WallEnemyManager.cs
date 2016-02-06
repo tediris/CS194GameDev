bb56be5e16ef3874fc47548a9d82abb223998a8e
Andrew Lamb <lamb@cs.stanford.edu>
2016-02-06 11:43:00 -0800

    Added WallEnemy.

---
+++ Assets/WallEnemyManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallEnemyManager : MonoBehaviour {
+
+	Vector2 velocity;
+	Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+		velocity = new Vector2 (0.0f, -0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidBody.velocity = velocity;
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			Destroy (other.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		velocity.y = -velocity.y;
+	}
+}
