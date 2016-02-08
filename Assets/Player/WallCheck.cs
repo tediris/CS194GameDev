6ce4ee8da4a54546d188dcdd06d576206f0ca011
Thaminda Edirisooriya <tediris@stanford.edu>
2016-02-08 10:26:55 -0800

    fixed teleporting, and wall climbing

--- Assets/Player/WallCheck.cs
+++ Assets/Player/WallCheck.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class WallCheck : MonoBehaviour {
 
 	public bool touchingWall = false;
 	public PlayerControl masterController;
+	HashSet<Collider2D> walls;
 	// Use this for initialization
 	void Start () {
 		masterController = GetComponentInParent<PlayerControl> ();
+		walls = new HashSet<Collider2D> ();
 	}
 	
 	public bool TouchingWall() {
@@ -19,6 +22,7 @@ public class WallCheck : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		touchingWall = true;
 		wall = other;
+		walls.Add (other);
 		if (!masterController.grounded) {
 			masterController.grabbingWall = true;
 			masterController.SetGravity (false);
@@ -26,6 +30,9 @@ public class WallCheck : MonoBehaviour {
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
+		walls.Remove (other);
+		if (walls.Count > 0)
+			return;
 		touchingWall = false;
 		if (masterController.grabbingWall) {
 			masterController.grabbingWall = false;
--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -18,7 +18,9 @@ public class Teleport : MonoBehaviour {
 //	}
 
 	void OnTriggerEnter2D (Collider2D other) {
-		other.transform.position = new Vector3 (toX, toY, other.transform.position.z);
-		Camera.main.transform.position = new Vector3 (toX, toY, Camera.main.transform.position.z);
+		if (other.tag == "Player") {
+			other.transform.position = new Vector3 (toX, toY, other.transform.position.z);
+			Camera.main.transform.position = new Vector3 (toX, toY, Camera.main.transform.position.z);
+		}
 	}
 }
