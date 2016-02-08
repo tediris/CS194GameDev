e13ca344cf61552b357190a263e4c8496b74ee01
Thaminda Edirisooriya <tediris@stanford.edu>
2016-02-08 01:06:25 -0800

    Big changes to player movement to make it snappier

---
+++ Assets/Player/WallCheck.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCheck : MonoBehaviour {
+
+	public bool touchingWall = false;
+	public PlayerControl masterController;
+	// Use this for initialization
+	void Start () {
+		masterController = GetComponentInParent<PlayerControl> ();
+	}
+	
+	public bool TouchingWall() {
+		return touchingWall;
+	}
+
+	Collider2D wall = null;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		touchingWall = true;
+		wall = other;
+		if (!masterController.grounded) {
+			masterController.grabbingWall = true;
+			masterController.SetGravity (false);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		touchingWall = false;
+		if (masterController.grabbingWall) {
+			masterController.grabbingWall = false;
+			masterController.SetGravity (true);
+		}
+	}
+}
