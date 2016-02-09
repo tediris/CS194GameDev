75decf0b546dd4116ae5c0bcea305fdb7a71157b
Thaminda Edirisooriya <tediris@stanford.edu>
2016-02-08 22:39:55 -0800

    trying to make networking with new levels work

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : NetworkBehaviour {
 
 	// Use this for initialization
 	public void MoveTo(float x, float y) {
-		transform.position = new Vector3 (x, y, transform.position.z);
+		if (isLocalPlayer) {
+			transform.position = new Vector3 (x, y, transform.position.z);
+		}
 	}
 }
