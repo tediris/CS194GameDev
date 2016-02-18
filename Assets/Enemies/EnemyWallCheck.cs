835b832d58171a50744e92212d0f2b371732aaca
Thaminda Edirisooriya <tediris@stanford.edu>
2016-02-18 12:46:08 -0800

    Added a dinosaur enemy that will walk back and forth

---
+++ Assets/Enemies/EnemyWallCheck.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWallCheck : MonoBehaviour {
+
+	public bool touchingWall = false;
+	public int numTouching = 0;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		numTouching++;
+		touchingWall = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		numTouching--;
+		if (numTouching < 1) {
+			touchingWall = false;
+			numTouching = 0;
+		}
+	}
+}
