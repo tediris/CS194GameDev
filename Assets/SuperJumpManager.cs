449ed68f60e8149e0eaeaab603f4e7287788e683
Andrew Lamb <lamb@cs.stanford.edu>
2016-02-21 20:01:32 -0800

    Added power jump (and possibly other stuff).

---
+++ Assets/SuperJumpManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SuperJumpManager : MonoBehaviour {
+
+	public Text superJumpModeText;
+
+	// Use this for initialization
+	void Start () {
+		superJumpModeText = GameObject.Find ("PowerJumpMode").GetComponent<Text> ();
+		superJumpModeText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			PlayerControl control = other.gameObject.GetComponent<PlayerControl> ();
+			control.hasJetpack = true;
+			Destroy (this.gameObject);
+		}
+	}
+}
